c8045a26fe5dc5e3627cd39a1588d1d2dfe53861
João Nascimento <joao.nascimento0576@gmail.com>
2021-08-03 14:42:22 -0300

    Alteração de nomes

--- public/js/contentbox_overflow.js
+++
@@ -1,6 +0,0 @@
-let container = document.getElementById('container')
-let content = container.getElementsByClassName('content-box')
-let itens = content.getElementsByClassName('descricao');
-let data = [].map.call(itens, item => item.textContent)
-
-console.log(data);
