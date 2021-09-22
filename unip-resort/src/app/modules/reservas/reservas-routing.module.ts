d97d44a04ae185cf0de96d78737cd639afe16efb
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-22 19:58:45 -0300

    Alterações

--- unip-resort/src/app/modules/reservas/reservas-routing.module.ts
+++ unip-resort/src/app/modules/reservas/reservas-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MinhasReservasComponent } from 'src/app/components/reservas/minhas-reservas/minhas-reservas.component';
+import { NovaReservaComponent } from 'src/app/components/reservas/nova-reserva/nova-reserva.component';
 import { PerfilComponent } from 'src/app/components/reservas/perfil/perfil.component';
 import { ReservasHomeComponent } from 'src/app/components/reservas/reservas-home/reservas-home.component';
 import { ReservasComponent } from 'src/app/components/reservas/reservas.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
     path: '', component: ReservasComponent, children: [
       { path: '', component: ReservasHomeComponent },
       { path: 'minhas-reservas', component: MinhasReservasComponent },
-      { path: 'meu-perfil', component: PerfilComponent }
+      { path: 'meu-perfil', component: PerfilComponent },
+      { path: 'nova-reserva', component: NovaReservaComponent }
     ]
   },
   {
--- unip-resort/src/app/modules/reservas/reservas.module.ts
+++ unip-resort/src/app/modules/reservas/reservas.module.ts
@@ -8,12 +8,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { PerfilComponent } from 'src/app/components/reservas/perfil/perfil.component';
 import { MinhasReservasComponent } from 'src/app/components/reservas/minhas-reservas/minhas-reservas.component';
 import { ReservasHomeComponent } from 'src/app/components/reservas/reservas-home/reservas-home.component';
+import { NovaReservaComponent } from 'src/app/components/reservas/nova-reserva/nova-reserva.component';
 
 @NgModule({
   declarations: [
     ReservasComponent,
     PerfilComponent,
     MinhasReservasComponent,
+    NovaReservaComponent,
     ReservasHomeComponent
   ],
   imports: [
