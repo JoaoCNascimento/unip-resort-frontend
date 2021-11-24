74c17d4369b206d02b2536c9ab768bebaf39360e
Joao Carlos Silva do Nascimento <joao.nascimento@concert.com.br>
2021-11-24 17:43:14 -0300

    Adição de melhores feedbacks de erro ao inserir e pesquisar ceps

--- unip-resort/src/app/services/cep.service.ts
+++ unip-resort/src/app/services/cep.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,23 @@ export class CepService {
   private readonly baseUrl = `https://viacep.com.br/ws/`;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private toastrService: ToastrService
   ) { }
 
   get(cep: string): Observable<any> {
-    return this.httpClient.get<any>(this.baseUrl + cep + '/json');
+    return this.httpClient.get<any>(this.baseUrl + cep + '/json').pipe(
+      tap(res => this.toastrService.success('CEP encontrado com sucesso!')),
+      catchError(er => {this.handleErrors(er); return er;})
+    );
+  }
+
+  handleErrors(er) {
+    if(er.erro)
+      this.toastrService.warning(er.erro);
+
+    if(er.status === 0)
+      this.toastrService.warning('Cep inválido.')
+
   }
 }
