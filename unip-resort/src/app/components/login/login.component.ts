bc5e4bf2039e611376a2abfe1b6357301020eb98
JoaoCNascimento <joao.nascimento0576@gmail.com>
2021-11-23 21:24:13 -0300

    Adição de método para geração de nova senha

--- unip-resort/src/app/components/login/login.component.ts
+++ unip-resort/src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Login } from 'src/app/models/Login';
 import { AuthService } from 'src/app/services/api/auth.service';
+import { ClienteService } from 'src/app/services/api/cliente.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +16,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private cs: ClienteService
   ) { }
 
   ngOnInit(): void {
@@ -24,12 +28,23 @@ export class LoginComponent implements OnInit {
   configurateForm() {
     return this.form = this.fb.group({
       email: [null, {validators: [
-        Validators.email
+        Validators.email,
+        Validators.required
       ]}],
       senha: [null, {validators: [
         Validators.required
       ]}],
     })
+  }
+
+  resetPassword() {
+    if(this.form.get('email').invalid)
+    {
+      this.toastrService.error('Digite um email para prosseguir com a solicitação.');
+      return this.form.get('email').markAsTouched();
+    }
+
+      this.cs.forgotPassword(this.form.get('email').value).subscribe();
   }
 
   onSubmit(){
