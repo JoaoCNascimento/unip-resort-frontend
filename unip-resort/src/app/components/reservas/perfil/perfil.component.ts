e3afd898ba713306fa13358c014888358bf4e907
JoaoCNascimento <joao.nascimento0576@gmail.com>
2021-11-23 21:25:10 -0300

    Adição de método para recuperação de dados do cliente logado

--- unip-resort/src/app/components/reservas/perfil/perfil.component.ts
+++ unip-resort/src/app/components/reservas/perfil/perfil.component.ts
@@ -1,63 +1,148 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { faEdit, faSave, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import {
+  faEdit,
+  faSave,
+  faTimes,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { Cliente } from "src/app/models/Cliente";
+import { ClienteService } from "src/app/services/api/cliente.service";
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  selector: "app-perfil",
+  templateUrl: "./perfil.component.html",
+  styleUrls: ["./perfil.component.css"],
 })
 export class PerfilComponent implements OnInit {
-
-  faTrash = faTrash
-  faEdit = faEdit
-  faSave = faSave
-  faTimes = faTimes
+  faTrash = faTrash;
+  faEdit = faEdit;
+  faSave = faSave;
+  faTimes = faTimes;
 
   form: FormGroup;
 
-  usuario = {
-    nome: "João Carlos",
-    cpf: "040.419.126-44",
-    rg: "10.412.414-5",
-    sexo: "M",
-    nascimento: "2002-03-24",
-    email: "joao@email.com",
-  }
+  cliente: Cliente = null;
 
   constructor(
     public fb: FormBuilder
-  ) {
-
-  }
+    ,private cs: ClienteService
+    ) {}
 
   ngOnInit(): void {
     this.configurarForm();
+    this.getCliente();
+  }
+
+  getCliente() {
+    this.cs.findByEmail().subscribe(
+      (res: Cliente) => {
+        res.dataNasc = res.dataNasc.split('/').reverse().join('-');
+        this.cliente = res;
+      this.configurarForm();}
+    );
   }
 
   configurarForm() {
+    if(this.cliente !== null) {
+      this.form = this.fb.group({
+        nome: new FormControl(this.cliente.nome, {
+          validators: [Validators.required],
+        }),
+        email: new FormControl(this.cliente.email, {
+          validators: [Validators.required, Validators.email],
+        }),
+        senha: new FormControl(this.cliente.senha, {
+          validators: [Validators.required],
+        }),
+        cpf: new FormControl(this.cliente.cpf, {
+          validators: [Validators.required],
+        }),
+        rg: new FormControl(this.cliente.rg, {
+          validators: [Validators.required],
+        }),
+        dataNasc: new FormControl(this.cliente.dataNasc, {
+          validators: [Validators.required],
+        }),
+        telefone: new FormControl(this.cliente.telefone, {
+          validators: [Validators.required],
+        }),
+        cep: new FormControl(this.cliente.bairro, {
+          validators: [Validators.required],
+        }),
+        rua: new FormControl(this.cliente.rua, {
+          validators: [Validators.required],
+        }),
+        bairro: new FormControl(this.cliente.bairro, {
+          validators: [Validators.required],
+        }),
+        numero: new FormControl(this.cliente.numero, {
+          validators: [Validators.required],
+        }),
+        cidade: new FormControl(this.cliente.cidade, {
+          validators: [Validators.required],
+        }),
+        estado: new FormControl(this.cliente.estado, {
+          validators: [Validators.required],
+        })
+      });
+
+      return this.form.disable();
+    }
     this.form = this.fb.group({
-      nome: new FormControl({ value: this.usuario.nome, disabled: true },
-        [Validators.required]),
-      cpf: new FormControl({ value: this.usuario.cpf, disabled: true },
-        [Validators.required]),
-      rg: new FormControl({ value: this.usuario.rg, disabled: true },
-        [Validators.required]),
-      sexo: new FormControl({ value: this.usuario.sexo, disabled: true },
-        [Validators.required]),
-      nascimento: new FormControl({ value: this.usuario.nascimento, disabled: true },
-        [Validators.required]),
-      email: new FormControl({ value: this.usuario.email, disabled: true },
-        [Validators.required, Validators.email])
+      nome: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      email: new FormControl(null, {
+        validators: [Validators.required, Validators.email],
+      }),
+      senha: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      cpf: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      rg: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      dataNasc: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      telefone: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      cep: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      rua: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      bairro: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      numero: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      cidade: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      estado: new FormControl(null, {
+        validators: [Validators.required],
+      })
     });
 
+    return this.form.disable();
   }
 
   ativarEdicao() {
     if (this.form.enabled) {
       this.form.disable();
-    }
-    else {
+    } else {
       this.form.enable();
     }
   }
