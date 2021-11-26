87ca17844fe4728faa8c867a3723ddeb7d165476
JoaoCNascimento <joao.nascimento0576@gmail.com>
2021-11-25 23:17:06 -0300

    Implementação de edição e exclusão de cadastro cliente

--- unip-resort/src/app/components/reservas/perfil/perfil.component.ts
+++ unip-resort/src/app/components/reservas/perfil/perfil.component.ts
@@ -5,14 +5,18 @@ import {
   FormGroup,
   Validators,
 } from "@angular/forms";
+import { Router } from "@angular/router";
 import {
   faEdit,
   faSave,
   faTimes,
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
+import * as moment from "moment";
+import { ToastrService } from "ngx-toastr";
 import { Cliente } from "src/app/models/Cliente";
 import { ClienteService } from "src/app/services/api/cliente.service";
+import { CepService } from "src/app/services/cep.service";
 
 @Component({
   selector: "app-perfil",
@@ -25,6 +29,8 @@ export class PerfilComponent implements OnInit {
   faSave = faSave;
   faTimes = faTimes;
 
+  editavel = false;
+
   form: FormGroup;
 
   cliente: Cliente = null;
@@ -32,11 +38,47 @@ export class PerfilComponent implements OnInit {
   constructor(
     public fb: FormBuilder
     ,private cs: ClienteService
+    ,private toastrService: ToastrService
+    ,private router: Router
+    ,private cepService: CepService
     ) {}
 
   ngOnInit(): void {
     this.configurarForm();
     this.getCliente();
+  }
+
+  deleteConta() {
+    if(confirm('Deseja realmente excluir sua conta?')){
+      this.cs.delete(this.cliente.id).subscribe(res => {
+        this.toastrService.info('Até a próxima!', '', { timeOut: 15000 })
+      })
+      return;
+    }
+    this.toastrService.info('Exclusão cancelada.');
+  }
+
+  onSubmitUpdate() {
+    if(!this.form.valid) {
+      this.form.markAllAsTouched();
+      return this.toastrService.warning('Verifique se todos os campos foram preenchidos corretamente.', 'Formulário inválido...', {
+        timeOut: 4000
+      });
+    }
+
+    if(this.form.get('senha').value === 'Digite sua senha, ou uma nova'){
+      this.toastrService.warning('Para salvar as alterações, digite sua senha ou uma nova.', '', {timeOut: 10000});
+      return this.toastrService.info('Para atualizar quaisquer campos é necessário atualizar, ou reinserir, a sua senha.', '', {timeOut: 10000});
+    }
+
+    let cliente: Cliente = Object.assign({}, this.form.value)
+
+    cliente.dataNasc = moment(cliente.dataNasc.toString()).format('DD/MM/yyyy');
+
+    this.cs.update(cliente).subscribe(res => {
+      this.getCliente();
+      this.ativarEdicao();
+    });
   }
 
   getCliente() {
@@ -51,6 +93,7 @@ export class PerfilComponent implements OnInit {
   configurarForm() {
     if(this.cliente !== null) {
       this.form = this.fb.group({
+        id: new FormControl(this.cliente.id),
         nome: new FormControl(this.cliente.nome, {
           validators: [Validators.required],
         }),
@@ -72,7 +115,7 @@ export class PerfilComponent implements OnInit {
         telefone: new FormControl(this.cliente.telefone, {
           validators: [Validators.required],
         }),
-        cep: new FormControl(this.cliente.bairro, {
+        cep: new FormControl(this.cliente.cep, {
           validators: [Validators.required],
         }),
         rua: new FormControl(this.cliente.rua, {
@@ -142,8 +185,30 @@ export class PerfilComponent implements OnInit {
   ativarEdicao() {
     if (this.form.enabled) {
       this.form.disable();
+      this.editavel = false;
+      this.toastrService.info('Formulário bloqueado para edição.');
     } else {
       this.form.enable();
+      this.editavel = true;
+      this.toastrService.info('Formulário editável.');
     }
+  }
+
+  findCep() {
+    this.cepService.get(this.form.get('cep').value).subscribe(res => this.changeAdressValues(res));
+  }
+
+  changeAdressValues(obj) {
+
+    console.log(obj);
+
+    if (obj.erro) {
+      return alert("Cep não encontrado");
+    }
+
+    this.form.get('rua').setValue(obj.logradouro);
+    this.form.get('bairro').setValue(obj.bairro);
+    this.form.get('cidade').setValue(obj.localidade);
+    this.form.get('estado').setValue(obj.uf);
   }
 }
